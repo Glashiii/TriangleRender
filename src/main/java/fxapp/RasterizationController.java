d5c55107ef01edf8e80bbf895f6bddc51c14ff82
Glashiii <glashyep@gmail.com>
2025-11-10 00:49:28 +0300

    Classes rename

--- src/main/java/fxapp/RasterizationController.java
+++ src/main/java/fxapp/RasterizationController.java
@@ -6,7 +6,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import triangle.TriangleRender;
+import triangle.Renderer;
 
 public class RasterizationController {
 
@@ -20,9 +20,9 @@ public class RasterizationController {
 
     @FXML
     private void initialize() {
-//        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
-//        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
-//        Triangle.test(canvas.getGraphicsContext2D());
+        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
+        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
+
         canvas.widthProperty().bind(anchorPane.widthProperty());
         canvas.heightProperty().bind(anchorPane.heightProperty());
         canvas.setOnMouseMoved(event -> {
@@ -32,11 +32,6 @@ public class RasterizationController {
             redrawScene();
         });
         Platform.runLater(this::redrawScene);
-
-//        Triangle.drawTriangle(200,200,Color.RED,
-//                    200, 350, Color.BLUE,
-//                150, 150, new Color(0,1,0,0),
-//                canvas.getGraphicsContext2D());
     }
 
     private void redrawScene() {
@@ -46,28 +41,28 @@ public class RasterizationController {
 
         PixelWriter pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
 
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                pixelWriter.setColor(x, y, Color.WHITE);
-//            }
-//        }
-//
-//
-//        Triangle.drawTriangle(200, 210, Color.RED,
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixelWriter.setColor(x, y, Color.WHITE);
+            }
+        }
+
+
+        Renderer.drawTriangle(200, 210, Color.RED,
+                210, 350, Color.BLUE,
+                x, y, new Color(0, 1, 0, 0),
+                canvas.getGraphicsContext2D());
+
+//        System.out.println("x: " + x + ", y: " + y);
+
+//        Renderer.drawTriangle(200, 210, Color.RED,
 //                210, 350, Color.BLUE,
-//                x, y, new Color(0, 1, 0, 0),
+//                534.4f, 206.8f, new Color(0, 1, 0, 0),
 //                canvas.getGraphicsContext2D());
-
-        System.out.println("x: " + x + ", y: " + y);
-
-        TriangleRender.drawTriangle(200, 210, Color.RED,
-                210, 350, Color.BLUE,
-                534.4f, 206.8f, new Color(0, 1, 0, 0),
-                canvas.getGraphicsContext2D());
-        TriangleRender.drawTriangle(520, 410, Color.RED,
-                210, 350, Color.BLUE,
-                534.4f, 206.8f, new Color(0, 1, 0, 0),
-                canvas.getGraphicsContext2D());
+//        Renderer.drawTriangle(520, 410, Color.RED,
+//                210, 350, Color.BLUE,
+//                534.4f, 206.8f, new Color(0, 1, 0, 0),
+//                canvas.getGraphicsContext2D());
     }
 
 
